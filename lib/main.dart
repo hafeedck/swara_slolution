ff8c40e2072a9f618efd9b42d24533460ec3b654
hafeedck <hafeed999@gmail.com>
2024-02-15 21:07:20 +0530

    hafeed

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Swara Solutions',
+      title: 'Sawara Solutions',
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
       debugShowCheckedModeBanner: false,
